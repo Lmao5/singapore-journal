83608ae37a7c561537eefe8341d2dd1091250a8a
Lmao5 <cilipadi270@gmail.com>
2022-08-07 21:08:48 +0800

    Added Open Sans font and initial CSS styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './Components/NavBar';
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>Hello World</h1>
     </div>
   );
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,11 @@
 import React from "react";
+import "./NavBar.css";
 
 function NavBar() {
     return (
-        <></>
+        <nav className="navbar">
+            <h1 className="nav-title">The Singapore Travel Journal</h1>
+        </nav>
     )
 }
 
