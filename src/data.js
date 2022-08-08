11fbda65d937512a1c3381711a4c06fbcb015fe3
Lmao5 <cilipadi270@gmail.com>
2022-08-08 21:16:55 +0800

    Added more information

--- src/data.js
+++ src/data.js
@@ -1,4 +1,6 @@
-import merlion from "./images/merlion.jpg"
+import merlion from "./images/merlion.jpg";
+import jewel from "./images/jewel.jpg";
+import gbtb from "./images/gbtb.jpg";
 
 export const cardData = [
     {
@@ -17,15 +19,31 @@ export const cardData = [
     },
     {
         id: 2,
-        title: "Merlion",
-        image: merlion,
-        image_alt: "Merlion",
-        location: "Merlion Park, Singapore",
-        gmaps_link: "https://www.google.com/maps/place/Merlion/@1.2867892,103.8525161,816m/data=!3m1!1e3!4m5!3m4!1s0x31da19085926dadb:0x9f427391c9f64c38!8m2!3d1.2867892!4d103.8545014",
-        visit_date: "07 August 2022",
-        description: `The Merlion statue is the national icon of Singapore. Located near the
-        mouth of the Singapore River, the half-lion and half-fish icon spews
-        water out from its mouth. Here, many tourists are found doing funny
-        poses in front of the statue.`
+        title: "Gardens by the Bay",
+        image: gbtb,
+        image_source: "https://unsplash.com/photos/THix6RDJ1Bw",
+        image_alt: "Gardens by the Bay",
+        location: "Marina Bay, Singapore",
+        gmaps_link: "https://www.google.com/maps/place/Gardens+by+the+Bay/@1.2817443,103.8524718,15.3z/data=!4m5!3m4!1s0x31da1904937e1633:0x62099677b59fca76!8m2!3d1.2815683!4d103.8636132",
+        visit_date: "25 June 2022",
+        description: `The Gardens by the Bay is a nature park located near the 
+        Central Business District in Singapore. This park helps to establish Singapore
+        as a 'City in a Garden' where people can live within nature. Take a stroll
+        in this park to enjoy the diverse range of plants that is not found 
+        locally in Singapore.`
+    },
+    {
+        id: 3,
+        title: "Jewel Changi Airport",
+        image: jewel,
+        image_source: "https://unsplash.com/photos/AfOQEnPCWz0",
+        image_alt: "Jewel Changi Airport",
+        location: "Changi Airport, Singapore",
+        gmaps_link: "https://www.google.com/maps/place/Jewel+Changi+Airport/@1.3577769,103.9879341,16.12z/data=!3m1!5s0x31da3c8963249ae3:0xc4789872c75c8dc0!4m5!3m4!1s0x31da17be147e79c3:0xb809d0f172526ac3!8m2!3d1.3602082!4d103.9897593",
+        visit_date: "05 July 2022",
+        description: `Upon landing in Changi Airport, you can visit the Jewel
+        which has the world's tallest indoor waterfall. Take picturesque shots 
+        of waterfall while enjoying the view of the indoor greenery at the airport. 
+        Surely this airport is more than just an airport.`
     }
 ]
